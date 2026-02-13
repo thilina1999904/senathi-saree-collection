a946620f22bf8912f6e78aa5e0777b0fbc81d0c3
Thilina Nirmal <thilinanirmalhl@gmail.com>
2026-02-13 13:18:13 +0530

    git ignore

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,6 +4,28 @@ export function AddProduct(req,res){
 
     console.log(req.user);
 
+    if(req.user == null){
+        res.status(401).json({
+            message: "Please Login and Try Again"
+        })
+        return
+
+    }
+
+    if(req.user.role != "admin"){
+        res.status(401).json({
+            message: "You are not authorized to perform this action"
+        })
+        return
+    }
+
+
+    if(req.user == null){
+        res.status(401).json({
+            message: "Please Login and Try Again"
+        })
+    }
+
     const data = req.body;
 
     const newProduct = new Product (data);
